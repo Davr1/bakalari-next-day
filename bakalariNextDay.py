9d82a79e2c47f97ef88ef3a4b6a2323270493acd
Davr1 <42148912+Davr1@users.noreply.github.com>
2022-02-16 19:56:49 +0100

    Update to Bakalari API v3

--- bakalariNextDay.py
+++ bakalariNextDay.py
@@ -1,75 +1,74 @@
 #!/usr/bin/env python3
 
 import requests
-import xml.etree.ElementTree as et
-import hashlib
-import base64
-import time
 import datetime
+import re
 
 class Bakalari: # Trida pro pristup k API bakalaru
     
     
-    def __init__(self, login, password, adresa): # Argumenty jsou string s prihlasovacim jmenem, string s heslem a string s url adresou bakalaru skoly. Ze zadanych udaju se vypocita token a ulozi jako self.token
+    def __init__(self, login, password, adresa): # Argumenty jsou string s prihlasovacim jmenem, string s heslem a string s url adresou bakalaru skoly. Zadane udaje se vyuziji pri ziskavani tokenu
         
-        # Adresa je potreba i v ostatnich metodach, heslo a prihlasovaci jmeno ne (staci token). 
-        self.adresa = adresa
+        # Odstrani prebytecny text z adresy a prida zpatky https protokol a /api endpoint
+        adresa = re.sub(r"(https?:\/\/)|(\/login)|(\/$)", "", adresa)
+        self.adresa = f"https://{adresa}/api"
         
-        # Ziskani zakladnich komponent pro token
-        loginInitR = requests.get(adresa, params = {"gethx": login})
-        loginInitX = et.fromstring(loginInitR.text)
-        typ = loginInitX.find("typ").text
-        ikod = loginInitX.find("ikod").text
-        salt = loginInitX.find("salt").text
-        
-        # Vypocet hesla
-        passwordHashOriginal = salt+ikod+typ+password
-        passwordHashOriginal = passwordHashOriginal.encode("UTF-8")
-        
-        # Zahashovani hesla
-        passwordHash = hashlib.sha512()
-        passwordHash.update(passwordHashOriginal)
-        passwordHash = base64.encodebytes(passwordHash.digest()).decode("UTF-8").replace("\n","")
-        
-        # Vypocet tokenu
-        tokenOriginal = "*login*" + login + "*pwd*" + passwordHash + "*sgn*ANDR" + time.strftime('%Y%m%d')
-        
-        # Zahashovani tokenu
-        token = hashlib.sha512()
-        token.update(tokenOriginal.encode("UTF-8"))
-        self.token = base64.encodebytes(token.digest()).decode("UTF-8").replace("\n","").replace("/","_").replace("\\","_").replace("+","-")
+        prihlasovaciUdaje = {
+            "client_id": "ANDR",
+            "grant_type": "password",
+            "username": login,
+            "password": password
+        }
+
+        # Ziska token ze serveru bakalaru
+        self.token = requests.post(
+            self.adresa + "/login",
+            headers={"Content-type": "application/x-www-form-urlencoded"},
+            data=prihlasovaciUdaje
+        ).json().get("access_token")
     
     
-    def rozvrh(self): # Vraci xml.etree.ElementTree.Element s rozvrhem a vytvori promenou scheduleXml se stejnym obsahem
-
-        # Ziskani rozvrhu
-        scheduleStr = requests.get(self.adresa, params = {"hx": self.token, "pm": "rozvrh"})
-        self.scheduleXml = et.fromstring(scheduleStr.text)
-        return (self.scheduleXml)
-
-class SkolniTyden(): # Trida pro zpracovavani XML z bakalaru
-
-
-    def __init__(self, scheduleXML): # Argument je XML s rozvrhem z Bakalaru
+    def rozvrh(self): # Vraci json s rozvrhem a vytvori promenou schedule se stejnym obsahem
         
+        # Ziska zitrejsi datum a zformatuje jej (YYYY-MM-DD) aby bylo pouzitelne v nasledujicim requestu
+        datum = (datetime.datetime.today() + datetime.timedelta(days=1)).strftime('%Y-%m-%d')
+
+        # Ziskani rozvrhu pro dane datum
+        self.schedule = requests.get(
+            self.adresa + "/3/timetable/actual",
+            headers={"Content-type": "application/x-www-form-urlencoded", "Authorization": f"Bearer {self.token}"},
+            params={"date": datum}
+        ).json()
+        return self.schedule
+
+class SkolniTyden(): # Trida pro zpracovavani json z bakalaru
+
+
+    def __init__(self, schedule): # Argument je json s rozvrhem z Bakalaru
+        
+        # Ziska nazvy predmetu a vytvori dict key id:nazev pro kazdy predmet
+        nazvyPredmetu = {}
+        predmety = schedule["Subjects"]
+        for predmet in predmety:
+            nazvyPredmetu[predmet["Id"]] = predmet["Name"]
+
         self.rozvrh = [] 
         cisloDnu = 0      
         
         # Pro kazdy den v rozvrhu se do self.rozvrh prida prazdne pole
-        dny = scheduleXML.find("rozvrh").find("dny").findall("den")
-        for den in dny:
-            hodiny = den.find("hodiny").findall("hod")
+        dny = list(den["Atoms"] for den in schedule["Days"])
+        for denniRozvrh in dny:
             self.rozvrh.append([])
             
             # Pro kazde pole (reprezentuje den ve skolnim rozvrhu) v self.rozvrh se do nej pridaji nazvy hodin, ktere v ten den jsou
-            for hodina in hodiny:
-                if hodina.find("pr") != None:
-                    self.rozvrh[cisloDnu].append(hodina.find("pr").text)
-                
+            for hodina in denniRozvrh:
+                if hodina["SubjectId"] != None:
+                    self.rozvrh[cisloDnu].append(nazvyPredmetu[hodina["SubjectId"]])
+            
             cisloDnu += 1
-    
-    
-    def vzitNa(self, den): # Int den 0 pro pondeli, 1 pro utery, ..., 4 pro patek. Vraci list se dvema listy, prvni obsahuje predmety co do tazky pridat, druhy co z ni vyndat
+
+
+    def vzitNa(self, den): # Vraci list se dvema listy, prvni obsahuje predmety co do tazky pridat, druhy co z ni vyndat
         
         # Pokud je pondeli, predchozi den je patek
         if den == 0:
@@ -85,7 +84,7 @@ class SkolniTyden(): # Trida pro zpracovavani XML z bakalaru
         for hodina in dnes:
             if hodina not in vcera:
                 pridat.append(hodina)
-                
+        
         for hodina in vcera:
             if hodina not in dnes:
                 odebrat.append(hodina)
@@ -98,12 +97,12 @@ class SkolniTyden(): # Trida pro zpracovavani XML z bakalaru
         odebrat = list(odebrat)
         
         return([pridat, odebrat])
-    
+
 # Ziskani udaju
 jmeno = input("Prihlasovaci jmeno: ")
 heslo = input("Heslo: ")
 adresa = input("URL adresa bakalaru skoly: ")
-    
+
 # Inicializace trid    
 uzivatel = Bakalari(jmeno, heslo, adresa)
 tyden = SkolniTyden(uzivatel.rozvrh())
